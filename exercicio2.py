a2088797478de7a47adce9591d4066446cdb5d2f
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-10-16 19:23:07 -0300

    adicionando informação sobre em qual lançamentos

--- exercicio2.py
+++ exercicio2.py
@@ -1,12 +1,13 @@
 from random import randint
 
 if __name__ == '__main__':
-    dados = [0 for _ in range(6)]
+    dados = [[0, []] for _ in range(6)]
 
-    for _ in range(100):
+    for i in range(1, 101):
         lancamento = randint(1, 6)
-        dados[lancamento - 1] += 1
+        dados[lancamento - 1][0] += 1
+        dados[lancamento - 1][1].append(str(i))
     
     for d in range(6):
-        print(f"O valor {d + 1} apareceu em {dados[d]} lançamentos!")
+        print(f"O valor {d + 1} apareceu em {dados[d][0]} lançamentos -> {', '.join(dados[d][1])}")
     
