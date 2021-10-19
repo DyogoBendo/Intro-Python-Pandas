d793cd60c2922e8601df322b14ac362b31772495
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-10-19 10:03:19 -0300

    Adequando formatação

--- exercicio5/main.py
+++ exercicio5/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_fwf('./usuarios.txt', header=None, names=['usuario', 'espaço utilizado'])
+df = pd.read_fwf('usuarios.txt', header=None, names=['usuario', 'espaço utilizado'])
 
 def byte_to_mega_byte(num):
     return round((num / (1024 * 1024)), 2)
@@ -9,21 +9,21 @@ espaco_total =df['espaço utilizado'].sum()
 df['% de uso'] = round((df['espaço utilizado']* 100 /espaco_total),2)
 
 df['espaço utilizado'] = byte_to_mega_byte(df['espaço utilizado'])
-print(df)
 
-relatorio = ('ACME Inc.            Uso do espaço em disco pelos usuário\n')
-relatorio += ('----------------------------------------------------------\n')
-relatorio += ('Nr. Usuário:      Espaço utilizado:       % de uso:\n')
+relatorio = 'ACME Inc.            Uso do espaço em disco pelos usuário\n'
+relatorio += '----------------------------------------------------------\n'
+relatorio += f'{"Nr.":6}{"Usuário:":20}{"Espaço utilizado:":20}{"% de uso:"}\n\n'
 
 x = 1
-for index, line in df.iterrows():
-    print(line)
-    relatorio += f'{x} {line["usuario"]} \t {line["espaço utilizado"]} MB \t {line["% de uso"]} %\n'
-    x= x+1
+for index, line in df.iterrows():    
+    relatorio += f'{x:<5} {line["usuario"]:<12}{str(line["espaço utilizado"]) + " MB":>21}{line["% de uso"]:>12} %\n'
+    x += 1
 
 relatorio += '\n'
 relatorio += f'Espaco total ocupado: {byte_to_mega_byte(espaco_total)} MB\n'
 relatorio += f'Espaco medio ocupado: {byte_to_mega_byte(espaco_total/6)} MB\n'
+
+print(relatorio)
 
 with open('relatorio.txt', 'w') as arquivo:
     arquivo.write(relatorio)
