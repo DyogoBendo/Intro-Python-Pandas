535b0134a769884c20d81ae40dcfef6ea487ea6b
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-10-19 12:39:20 -0300

    Adicionando impressão em html

--- exercicio5/README.md
+++ exercicio5/README.md
@@ -1,16 +1,20 @@
 # Como executar o programa
 Para executar o programa, dentro desta pasta, execute em seu terminal: 
 
-"""
-python main.py arg1 arg2
-"""
+```
+python main.py arg1 arg2 arg3
+```
 
-arg1: informe um número, sendo 0 se for para que o resultado saía em ordem crescente, e qualquer outro número para que seja em ordem descrescente
+arg1: informe um número, 0 ou 1. 0 a saída será em html, e 1 a saída será em txt. 
 
-arg2: informe um número, que é a quantidade máxima de usuários que será considerada
+arg2: informe um número, sendo 0 se for para que o resultado saía em ordem crescente, e qualquer outro número para que seja em ordem descrescente
+
+arg3: informe um número, que é a quantidade máxima de usuários que será considerada
 
 É possível executar sem nenhum argumento, ou com apenas um.
 
-No caso em que não há nenhum argumento, será considerada a ordem crescente, e todos os usuários.
+No caso em que não há nenhum argumento, o resultado será em html, a ordem crescente, e todos os usuários.
 
-Com apenas um argumento, ele define se será em ordem crescente ou descrescente, e todos os usuários serão considerados. 
+Com apenas um argumento, ele define o formato, sendo em ordem crescente e com todos os usuários considerados.  
+
+Com dois argumentos ele define o formato e se será em ordem crescente ou descrescente, e todos os usuários serão considerados. 
--- exercicio5/main.py
+++ exercicio5/main.py
@@ -1,5 +1,6 @@
+from posix import listdir
 import pandas as pd
-import sys
+import sys, os, webbrowser
 from math import inf
 
 def byte_to_mega_byte(num):
@@ -8,18 +9,102 @@ def byte_to_mega_byte(num):
 def calcula_porcentagem_uso(espaco_utilizado, espaco_total):
     return round(((espaco_utilizado * 100) / espaco_total), 2)
 
+
+def gera_cabecalho_html():
+    cabecalho = "<!DOCTYPE html>\n"
+    cabecalho += '<html lang="en">\n'
+    cabecalho += "<head>\n"
+    cabecalho += '\t<meta charset="UTF-8">\n'
+    cabecalho += '\t<link rel="stylesheet" href="style.css">\n'
+    cabecalho += '\t<title>Relatório</title>\n'
+    cabecalho += '</head>\n'
+    cabecalho += '<body>\n'
+
+    return cabecalho
+
+def gera_relatorio_html(df, numero_usuarios, espaco_total):    
+    relatorio = gera_cabecalho_html()
+    relatorio += '\t<header>\n'
+    relatorio += '\t\t<h1> ACME Inc. </h1>\n'
+    relatorio += '\t\t<h2> Uso do espaço em disco pelos usuários </h2>\n'
+    relatorio += '\t</header>\n'    
+    relatorio += '\t\t<hr>\n' 
+    relatorio += '\t<table>\n'
+    relatorio += '\t\t<tr>\n'
+    relatorio += '\t\t\t<th> Nr. </th>\n'
+    relatorio += '\t\t\t<th> Usuário </th>\n'
+    relatorio += '\t\t\t<th> Espaço Utilizado </th>\n'
+    relatorio += '\t\t\t<th> % de Uso </th>\n'
+    relatorio += '\t\t</tr>\n'        
+    
+    for index, line in df.iterrows():    
+        relatorio += '\t\t<tr>\n'
+        if numero_usuarios:
+            relatorio += f'\t\t\t<td> {index + 1} </th>\n'
+            relatorio += f'\t\t\t<td> {line["usuario"]} </th>\n'
+            relatorio += f'\t\t\t<td> {line["espaço utilizado"]} MB </th>\n'
+            relatorio += f'\t\t\t<td> {line["% de uso"]} % </th>\n'                        
+            numero_usuarios -= 1
+        else:
+            break
+        relatorio += '\t\t</tr>\n'
+
+    relatorio += '\t</table>\n'
+    relatorio += '\n'
+    relatorio += '\t<footer>\n'
+    relatorio += f'\t\t<h2>Espaço total ocupado: {byte_to_mega_byte(espaco_total)} MB</h2>\n'
+    relatorio += f'\t\t<h2>Espaço médio ocupado: {byte_to_mega_byte(espaco_total/6)} MB</h2>\n'
+    relatorio += '\t</footer>\n'
+
+    relatorio += '</body>\n'
+    relatorio += '</html>'
+    return relatorio
+
+def gera_relatorio_txt(df, numero_usuarios, espaco_total):
+    relatorio = 'ACME Inc.            Uso do espaço em disco pelos usuário\n'
+    relatorio += '----------------------------------------------------------\n'
+    relatorio += f'{"Nr.":6}{"Usuário:":20}{"Espaço utilizado:":20}{"% de uso:"}\n\n'    
+    for index, line in df.iterrows():    
+        if numero_usuarios:
+            relatorio += f'{index+1:<5} {line["usuario"]:<12}{str(line["espaço utilizado"]) + " MB":>21}{line["% de uso"]:>12} %\n'            
+            numero_usuarios -= 1
+        else:
+            break
+
+    relatorio += '\n'
+    relatorio += f'Espaco total ocupado: {byte_to_mega_byte(espaco_total)} MB\n'
+    relatorio += f'Espaco medio ocupado: {byte_to_mega_byte(espaco_total/6)} MB\n'
+    return relatorio
+
+def gera_relatorio(df, numero_usuarios, espaco_total, tipo):    
+    if tipo == 0:
+        relatorio = gera_relatorio_html(df, numero_usuarios, espaco_total)
+        with open('index.html', 'w') as arquivo:
+            arquivo.write(relatorio)                
+        webbrowser.open(url="index.html", new=1)
+    else:
+        relatorio = gera_relatorio_txt(df, numero_usuarios, espaco_total)
+        with open('relatorio.txt', 'w') as arquivo:
+            arquivo.write(relatorio)        
+        webbrowser.open(url="relatorio.txt", new=1)
+
 if __name__ == "__main__":
-
+    
     argv = list(map(int, sys.argv[1:]))
+    
+    tipo = 0
+    ordem = True
+    numero_usuarios = inf
 
-    if not argv:
-        ordem = True
-        numero_usuarios = inf
+    if len(argv) >= 1:
+        tipo = argv[0]
 
-    elif len(argv) >= 1:
-        ordem = False if argv[0] else True
-        if len(argv) == 2:        
-            numero_usuarios = argv[1]    
+        if len(argv) >= 2:        
+            ordem = False if argv[1] else True
+
+        if len(argv) == 3:
+            numero_usuarios = argv[2]
+
 
     df = pd.read_fwf('usuarios.txt', header=None, names=['usuario', 'espaço utilizado'])
     espaco_total= df['espaço utilizado'].sum()
@@ -29,22 +114,5 @@ if __name__ == "__main__":
 
     df = df.sort_values('% de uso', ascending=ordem)  # ordenando pelo percentual de uso
 
-    relatorio = 'ACME Inc.            Uso do espaço em disco pelos usuário\n'
-    relatorio += '----------------------------------------------------------\n'
-    relatorio += f'{"Nr.":6}{"Usuário:":20}{"Espaço utilizado:":20}{"% de uso:"}\n\n'
-
-    x = 1    
-    for index, line in df.iterrows():    
-        if numero_usuarios:
-            relatorio += f'{x:<5} {line["usuario"]:<12}{str(line["espaço utilizado"]) + " MB":>21}{line["% de uso"]:>12} %\n'
-            x += 1
-            numero_usuarios -= 1
-        else:
-            break
-
-    relatorio += '\n'
-    relatorio += f'Espaco total ocupado: {byte_to_mega_byte(espaco_total)} MB\n'
-    relatorio += f'Espaco medio ocupado: {byte_to_mega_byte(espaco_total/6)} MB\n'
-
-    with open('relatorio.txt', 'w') as arquivo:
-        arquivo.write(relatorio)
+    gera_relatorio(df, numero_usuarios, espaco_total, tipo)
+        
